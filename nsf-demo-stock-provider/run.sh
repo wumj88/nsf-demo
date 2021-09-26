82c5702d58261852b7f357e34e32c24104c71961
wangbicheng <wangbicheng@corp.netease.com>
2021-09-26 11:32:15 +0800

    change dockerfile

--- nsf-demo-stock-provider/run.sh
+++ nsf-demo-stock-provider/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -javaagent:/opt/nsf/nsf-agent.jar=nsf -javaagent:/opt/apm/napm-java-agent/napm-java-rewriter.jar=conf=napm-agent.properties -Dnsf.host.enhance=true -Dsun.net.http.allowRestrictedHeaders=true -jar /opt/nsf/nsf-demo-stock-provider-1.0-SNAPSHOT.jar
+java -javaagent:/opt/nsf/nsf-agent.jar=nsf -Dnsf.host.enhance=true -Dsun.net.http.allowRestrictedHeaders=true -jar /opt/nsf/nsf-demo-stock-provider-1.0-SNAPSHOT.jar
