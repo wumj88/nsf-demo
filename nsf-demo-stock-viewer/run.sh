08b8a24437acfa02faf2abc7d33f0c0248894b28
dw <deathwishlab@163.com>
2022-03-05 12:16:37 +0800

    modify again

--- nsf-demo-stock-viewer/run.sh
+++ nsf-demo-stock-viewer/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -javaagent:/opt/nsf/nsf-agent.jar=nsf -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
+java -javaagent:/opt/nsf/nsf-agent.jar=nsf -javaagent:/opt/apm/napm-java-agent/agent/skywalking-agent.jar -Dskywalking_config=/opt/apm/napm-java-agent/agent/config/agent.config -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
