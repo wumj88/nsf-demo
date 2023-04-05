14a66493ac653db5c61adaf475e11e610d3d0673
deathwishlab <deathwishlab@163.com>
2023-04-05 16:13:11 +0800

    Update run.sh

--- nsf-demo-stock-viewer/run.sh
+++ nsf-demo-stock-viewer/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -javaagent:/opt/nsf/nsf-agent.jar=nsf -Dstock_provider_url=http://dw-sm-stock-provider -Dstock_advisor_url=http://dw-sm-stock-advisor -javaagent:/opt/apm/skywalking-napm-bin-8.5.0-latest/agent/skywalking-agent.jar -Dskywalking_config=/opt/apm/skywalking-napm-bin-8.5.0-latest/agent/config/agent.config -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
+java -javaagent:/opt/nsf/nsf-agent.jar=nsf -Dstock_provider_url=http://nsf-provider -Dstock_advisor_url=http://nsf-advisor -javaagent:/opt/apm/skywalking-napm-bin-8.10.0-latest/agent/skywalking-agent.jar -Dskywalking_config=/opt/apm/skywalking-napm-bin-8.10.0-latest/agent/config/agent.config -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
