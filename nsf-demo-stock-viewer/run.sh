0d6da0479df5c2e00e29d76000286e3a47ce9e29
deathwishlab <deathwishlab@163.com>
2023-09-14 19:54:40 +0800

    Update run.sh

--- nsf-demo-stock-viewer/run.sh
+++ nsf-demo-stock-viewer/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java JAVA_OPTS -Dstock_provider_url=http://nsf-provider -Dstock_advisor_url=http://nsf-advisor -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
+java $JAVA_OPTS -Dstock_provider_url=http://nsf-provider -Dstock_advisor_url=http://nsf-advisor -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
