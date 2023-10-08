9012a77078eb577fe2fa4b58c1cd7465d3678ae1
wumj <wumingjun@corp.netease.com>
2023-10-08 20:17:11 +0800

    1

--- nsf-demo-stock-viewer/run.sh
+++ nsf-demo-stock-viewer/run.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 java -javaagent:/opt/nsf/nsf-agent-fat.jar=nsf -Dnsf.grpc.timeout=15000 -Dstock_provider_url=http://nsf-provider -Dstock_advisor_url=http://nsf-advisor -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
